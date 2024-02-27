c14fc14b7f4e6f77e9e4d3a5fe927d4bdb0f83e8
Kevin Ammerman <127098309+KevinAmmerman@users.noreply.github.com>
2024-02-27 23:29:26 +0100

    error handling http request

--- src/app/shared/services/geo-location-service.service.ts
+++ src/app/shared/services/geo-location-service.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { catchError, of, retry } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,12 @@ export class GeoLocationServiceService {
 
   searchLocation(query: string) {
     const url = `https://api.geoapify.com/v1/geocode/search?text=${query}&lang=de&limit=10&type=city&apiKey=1f0cbc28e50e4db19cb831745c5953a2`;
-    return this.http.get(url);
+    return this.http.get(url).pipe(
+      retry(3),
+      catchError(error => {
+        console.error('An error has occurred', error);
+        return of([]);
+      })
+    );
   }
 }
--- src/app/shared/services/weather-data.service.ts
+++ src/app/shared/services/weather-data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { catchError, of, retry } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,12 @@ export class WeatherDataService {
 
   getWeatherData(lon: number, lat: number) {
     const url = `https://api.pirateweather.net/forecast/sOgfd7Inh9o7rzJ3XBYr0MjZ9hA6vWbu/${lat}%2C${lon}?exclude=minutely%2C+hourly%2C+alerts&units=ca`
-    return this.http.get(url);
+    return this.http.get(url).pipe(
+      retry(3),
+      catchError(error => {
+        console.error('An error has occurred', error);
+        return of([])
+      })
+    );
   }
 }
