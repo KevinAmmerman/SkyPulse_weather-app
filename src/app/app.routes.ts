decbed1b0106a16d8ce101bb05e9e70b187baaeb
Kevin Ammerman <127098309+KevinAmmerman@users.noreply.github.com>
2024-03-03 12:46:09 +0100

    created component for saved locations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { SavedLocationsComponent } from './saved-locations/saved-locations.component';
 
 export const routes: Routes = [
     { path: '', component: DashboardComponent },
+    { path: 'saved-locations', component: SavedLocationsComponent }
 ];
---
+++ src/app/saved-locations/saved-locations.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saved-locations',
+  standalone: true,
+  imports: [],
+  templateUrl: './saved-locations.component.html',
+  styleUrl: './saved-locations.component.scss'
+})
+export class SavedLocationsComponent {
+
+}
